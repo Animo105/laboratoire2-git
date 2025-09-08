bb8b78446e74285ac2df6bdc41acde955a03f75f
Animo105 <emeric2005leclerc@gmail.com>
2025-09-08 09:41:34 -0400

    projet upgrade

--- file_accesser.py
+++ file_accesser.py
@@ -1,12 +1,34 @@
 import secret
+import os
 
-print("Entrez le code secret: ")
+
+
+print("enter secret code: ")
 code = input()
 
 if code != secret.mon_code_super_secret:
-    print("Mauvais code entré.")
+    print("wrong code entered.")
 
 elif code == secret.mon_code_super_secret:
-    print("contenu du fichier secret:")
-    f = open("fichier_secret.txt")
-    print(f.read())
+
+    # check if file exist
+    if os.path.exists(secret.secret_file_path):
+        # read the file
+        print("secret file content:")
+        with open(secret.secret_file_path, "r") as f:
+            print(f.read())
+        # propose to update the file
+        print("\nOverride new text? (n/y): ")
+        a = input()
+        if a == "y" or a == "Y":
+            print("enter new text:")
+            text = input()
+            with open(secret.secret_file_path, "w") as f:
+                f.write(text)
+    
+    # create the file
+    else:
+        print("no secret file found, enter text to secret file:")
+        text = input()
+        with open(secret.secret_file_path, "w") as f:
+            f.write(text)
